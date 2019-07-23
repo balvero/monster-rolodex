ef322fe30c694c6c09f462315a5b408059aafdc6
Bernard Alvero <bernard.alvero@gmail.com>
2019-07-23 15:17:41 +0800

    Updates

---
+++ precache-manifest.83730b6924f5beec813ec2c3e4f15d48.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f537c4dbf0260d624161bfedec72f655",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "2d9bccae9242f3654635",
+    "url": "/monster-rolodex/static/css/main.e6118725.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/monster-rolodex/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "2d9bccae9242f3654635",
+    "url": "/monster-rolodex/static/js/main.f067aeaa.chunk.js"
+  },
+  {
+    "revision": "344166cfeaf30c2159bd",
+    "url": "/monster-rolodex/static/js/runtime~main.5df944a8.js"
+  }
+]);
